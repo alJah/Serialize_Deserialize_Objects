c150f21cf20b2e5f85fc53b7c91930de1f630f8b
Roman <mn.07.7@mail.ru>
2020-01-07 08:00:19 +0700

    Add files via upload

---
+++ OpenSaveObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/// <summary>
+/// Serializes, deserializes objects like Array, List <>, Dictionary <,>, Tuple, etc.
+/// </summary>
+public static class OpenSaveObject
+{
+
+    /// <summary>
+    /// Save object to file.
+    /// </summary>
+    /// <param name="fName"></param>
+    /// <param name="objToSerialize"></param>
+    public static void Serialize(string fName, object objToSerialize)
+    {
+        try
+        {
+            using (FileStream fileStream = new FileStream(fName, FileMode.Create, FileAccess.Write, FileShare.Read))
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(fileStream, objToSerialize);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    /// <summary>
+    /// Open an object from a file.
+    /// </summary>
+    /// <returns></returns>
+    public static object Deserializea(string fileName)
+    {
+        object myObject;
+
+        using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            myObject = binaryFormatter.Deserialize(fileStream);
+        }
+
+        return myObject;
+    }
+}
